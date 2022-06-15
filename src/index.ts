46f60261a716e74858eaa1be93157fc4735ead2b
volKate <ekaterina.volkova@itechart-group.com>
2022-06-15 13:20:09 +0300

    feat: add request listener with user controller

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import dotenv from "dotenv";
 import http from "http";
+import { userController } from "./controllers";
 
 dotenv.config();
 
-const server = http.createServer();
+const URLS = {
+  user: "/api/user",
+};
+
+const requestListener: http.RequestListener = (req, res) => {
+  if (req.url?.startsWith(URLS.user)) {
+    userController(req, res);
+    return;
+  }
+
+  // if there is no controller for requested url end with an error
+  res.statusCode = 404;
+  res.end("Requested resource is not found");
+};
+
+const server = http.createServer(requestListener);
 
 const port = process.env.PORT || 8004;
 
